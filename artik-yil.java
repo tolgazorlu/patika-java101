cba5b7b7d6f103322e79cc845d1f2fbc7befe6f2
Tolga Zorlu <44470351+tolgazorlu@users.noreply.github.com>
2023-03-04 18:56:00 +0300

    Create artik-yil.java

---
+++ artik-yil.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Yıl giriniz : ");
+        int yil = input.nextInt();
+
+        if(((yil % 4 == 0) && (yil % 100 == 0)) || (yil % 400 == 0)){
+            System.out.print(yil + " bir artık yıldır!");
+        }
+        else{
+            System.out.print(yil + " bir artık yıl değildir!");
+        }
+
+    }
+}
