4473f3bab85d168a726ab1e7d3971a9fd1c2b42e
Tolga Zorlu <44470351+tolgazorlu@users.noreply.github.com>
2023-03-04 18:39:17 +0300

    Create cin-zodyagi.java

---
+++ cin-zodyagi.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Doğum yılınızı giriniz : ");
+        int dt = input.nextInt();
+
+        int mod = dt % 12;
+
+        switch (mod){
+            case 0:{
+                System.out.print("Maymun");break;
+            }case 1:{
+                System.out.print("Horoz");break;
+            }case 2:{
+                System.out.print("Köpek");break;
+            }case 3:{
+                System.out.print("Domuz");break;
+            }case 4:{
+                System.out.print("Fare");break;
+            }case 5:{
+                System.out.print("Öküz");break;
+            }case 6:{
+                System.out.print("Kaplan");break;
+            }case 7:{
+                System.out.print("Tavşan");break;
+            }case 8:{
+                System.out.print("Ejderha");break;
+            }case 9:{
+                System.out.print("Yılan");break;
+            }case 10:{
+                System.out.print("At");break;
+            }case 11:{
+                System.out.print("Koyun");break;
+            }
+
+
+        }
+
+    }
+}
